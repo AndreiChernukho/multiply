9bf05ebc40584e573fdd7f1678f22bd7aadd35a3
sol2101 <sol2101@yandex.ru>
2019-10-13 14:14:27 +0300

    feat: implement function multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let result = [];
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      let multiplication = first[i] * second[j]
+      result[i + j] = result[i + j] ? result[i + j] + multiplication : multiplication;
+    }
+  }
+
+  for (let i = result.length; i > 0; i--) {
+    if (result[i] > 9) {
+      let number = Math.floor(result[i] / 10);
+      result[i] = result[i] % 10;
+      result[i - 1] = result[i - 1] + number;
+    }
+  }
+
+  return result.join("");
 }
